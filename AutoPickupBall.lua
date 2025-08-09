c0a2d0f2ee2bb3063348633d1274b0a811828d0f
percwalkkk <110597529+percwalkkk@users.noreply.github.com>
2025-08-09 13:03:34 -0400

    Update AutoPickupBall.lua

--- AutoPickupBall.lua
+++ AutoPickupBall.lua
@@ -1,1 +1,20 @@
+local Players = game:GetService("Players")
+local RunService = game:GetService("RunService")
 
+local LocalPlayer = Players.LocalPlayer
+local UserInputService = game:GetService("UserInputService")
+
+
+RunService.RenderStepped:Connect(function()
+    if getgenv().AutoPickupBall then
+if game:GetService("Players").LocalPlayer.Values.InGame.Value ~= true then
+        if game:GetService("Players").LocalPlayer.Values.Basketball == nil then
+
+		currentPos = game.Players.LocalPlayer.Character.HumanoidRootPart.CFrame
+		game.Players.LocalPlayer.Character.HumanoidRootPart.CFrame = workspace.Practice_Court.BallRack.CFrame; wait(2)
+		fireproximityprompt(workspace.Practice_Court.BallRack.ProximityPrompt); wait()
+		game.Players.LocalPlayer.Character.HumanoidRootPart.CFrame = currentPos
+		end
+    end
+    end
+  end)
