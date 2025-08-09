48986fa845d064ee84cd86b952d7dcd8b87537e2
percwalkkk <110597529+percwalkkk@users.noreply.github.com>
2025-08-09 10:47:15 -0400

    Create DribbleGlide.lua

---
+++ DribbleGlide.lua
@@ -0,0 +1,20 @@
+local Players = game:GetService("Players")
+local RunService = game:GetService("RunService")
+
+local LocalPlayer = Players.LocalPlayer
+local UserInputService = game:GetService("UserInputService")
+
+
+RunService.RenderStepped:Connect(function()
+if getgenv().DribbleGlide then
+        if game:GetService("Players").LocalPlayer.Values.Basketball.Value ~= nil then
+        game.Players.LocalPlayer.Character.Humanoid:Move(Vector3.new(0,0,-1), true) -- Always forward relative to camera
+        local camCF = workspace.CurrentCamera.CFrame
+        game.Players.LocalPlayer.Character.HumanoidRootPart.CFrame = game.Players.LocalPlayer.Character.HumanoidRootPart.CFrame:Lerp(
+            CFrame.new(game.Players.LocalPlayer.Character.HumanoidRootPart.Position, game.Players.LocalPlayer.Character.HumanoidRootPart.Position + camCF.LookVector),
+            0.25
+        )
+        UpdateDribble(dt)
+        end
+    end
+  end)
