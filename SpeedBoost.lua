93b5f5d16d4f00febdd42279f23b06d049677462
percwalkkk <110597529+percwalkkk@users.noreply.github.com>
2025-08-09 10:50:37 -0400

    Create SpeedBoost.lua

---
+++ SpeedBoost.lua
@@ -0,0 +1,12 @@
+local Players = game:GetService("Players")
+local RunService = game:GetService("RunService")
+
+local LocalPlayer = Players.LocalPlayer
+local UserInputService = game:GetService("UserInputService")
+
+RunService.RenderStepped:Connect(function()
+if getgenv().SpeedBost and char:FindFirstChild("Humanoid") and char.Humanoid.MoveDirection.Magnitude > 0 then
+        local moveDirection = char.Humanoid.MoveDirection
+        hrp.AssemblyLinearVelocity = moveDirection * char.Humanoid.WalkSpeed * getgenv().SpeedMultiplier + Vector3.new(0, hrp.AssemblyLinearVelocity.Y, 0)
+    end
+  end)
