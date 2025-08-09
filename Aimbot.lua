fad5a904509a5149f7a419375a85fc520fe1628f
percwalkkk <110597529+percwalkkk@users.noreply.github.com>
2025-08-09 10:50:58 -0400

    Create Aimbot.lua

---
+++ Aimbot.lua
@@ -0,0 +1,18 @@
+local Players = game:GetService("Players")
+local RunService = game:GetService("RunService")
+
+local LocalPlayer = Players.LocalPlayer
+local UserInputService = game:GetService("UserInputService")
+
+UserInputService.InputBegan:Connect(function(input, gameProcessed)
+	if gameProcessed then return end
+	if getgenv().Aimbot then
+	if input.KeyCode == Enum.KeyCode.E then
+		getgenv().Aimbot = {toggled = true, releaseTime = -1}
+
+		local aimbotReleaseArguments = {true, 100, [4] = false}
+		game:GetService("ReplicatedStorage"):WaitForChild("Events"):WaitForChild("Shoot"):FireServer(unpack(aimbotReleaseArguments))
+		game:GetService("ReplicatedStorage"):WaitForChild("Events"):WaitForChild("Shoot"):FireServer(false, Aimbot.releaseTime, false)
+	end else return
+	end
+end)
