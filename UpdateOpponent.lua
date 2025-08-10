2b647ab674a307becf4017427518e5f1baa93661
percwalkkk <110597529+percwalkkk@users.noreply.github.com>
2025-08-10 14:32:46 -0400

    Create UpdateOpponent.lua

---
+++ UpdateOpponent.lua
@@ -0,0 +1,26 @@
+local courts = workspace.Courts;
+    local findOpponent = function()
+    local values = {court, opponent};
+   for __, court in pairs (game.Workspace.Courts:GetDescendants()) do
+   if string.find(court.Name, 'Player') and court.Value == game.Players.LocalPlayer.Name then
+    values.court = court.Parent.Parent.Name
+
+    if court.Name == 'Player2' then
+    values.opponent = game.Players[court.Parent.Player1.Value]
+        elseif court.Name == 'Player1' then
+        values.opponent = game.Players[court.Parent.Player2.Value]
+            end
+        end
+    end
+    return values
+end
+
+getgenv().currentOpponent = findOpponent().opponent
+getgenv().currentCourt = findOpponent().court
+
+game:GetService("Players").LocalPlayer.Values.InGame.Changed:Connect(functon()
+  if game:GetService("Players").LocalPlayer.Values.InGame.Value == true then
+  getgenv().currentOpponent = findOpponent().opponent
+  getgenv().currentCourt = findOpponent().court
+  end
+end)
