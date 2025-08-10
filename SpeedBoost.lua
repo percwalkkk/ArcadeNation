5e553b87b3f9bd82a9f28e196505f0a09ad2447e
percwalkkk <110597529+percwalkkk@users.noreply.github.com>
2025-08-10 03:12:40 -0400

    Update SpeedBoost.lua

--- SpeedBoost.lua
+++ SpeedBoost.lua
@@ -1,12 +1,13 @@
-local Players = game:GetService("Players")
-local RunService = game:GetService("RunService")
+local player = game.Players.LocalPlayer
+local char = player.Character or player.CharacterAdded:Wait()
+local hrp = char:WaitForChild("HumanoidRootPart")
+local humanoid = char:WaitForChild("Humanoid")
+local runService = game:GetService("RunService")
+local uis = game:GetService("UserInputService")
 
-local LocalPlayer = Players.LocalPlayer
-local UserInputService = game:GetService("UserInputService")
-
-RunService.RenderStepped:Connect(function()
-if getgenv().SpeedBoost and char:FindFirstChild("Humanoid") and char.Humanoid.MoveDirection.Magnitude > 0 then
-        local moveDirection = char.Humanoid.MoveDirection
-        hrp.AssemblyLinearVelocity = moveDirection * char.Humanoid.WalkSpeed * getgenv().SpeedMultiplier + Vector3.new(0, hrp.AssemblyLinearVelocity.Y, 0)
-    end
-  end)
+runService.RenderStepped:Connect(function()
+	if getgenv().SpeedBoost and humanoid.MoveDirection.Magnitude > 0 then
+		-- Move the HRP manually using CFrame
+		hrp.CFrame = hrp.CFrame + (humanoid.MoveDirection * getgenv().SpeedMultiplier)
+	end
+end)
